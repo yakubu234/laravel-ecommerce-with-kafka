db78b7bb311190df3707f220e71fed168658ea1d
yakubu234 <yakubuabiola2003@gmail.com>
2023-04-18 14:51:46 +0100

    adding the controllers and models

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function addItemsToCart()
+    {
+    }
+
+    public function removeItemsFromCart()
+    {
+    }
+
+    public function getAllItems()
+    {
+    }
 }
---
+++ database/migrations/2023_04_18_134430_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('img_src');
+            $table->string('name');
+            $table->text('description');
+            $table->string('img_src');
+            $table->string('price');
+            $table->integer('in_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
